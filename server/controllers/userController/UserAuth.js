ef4c9679c33f8fd1cd98544d4501a270685698d6
HardikMutha <hardikmutha2005@gmail.com>
2025-01-18 02:51:05 +0530

    Fix: Login Routes which were modified due to conflict

--- server/controllers/userController/UserAuth.js
+++ server/controllers/userController/UserAuth.js
@@ -4,123 +4,126 @@ const bcrypt = require("bcryptjs");
 const { loginSchema, signUpSchema } = require("./Validation");
 const saltRounds = 10;
 
-const createToken = (id) => {
-	return jwt.sign(id, process.env.JWTSECRET, { expiresIn: 24 * 60 * 60 });
+const createToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWTSECRET, {
+    expiresIn: 24 * 60 * 60,
+  });
 };
 
 const hashPassword = async (password) => {
-	try {
-		const hash = await bcrypt.hash(password, saltRounds);
-		if (!hash) return;
-		return hash;
-	} catch (err) {
-		console.log(err);
-		return;
-	}
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    if (!hash) return;
+    return hash;
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 const matchPassword = async (password, hashed_password) => {
-	try {
-		const result = await bcrypt.compare(password, hashed_password);
-		return result;
-	} catch (err) {
-		console.log("Invalid Password");
-		return false;
-	}
+  try {
+    const result = await bcrypt.compare(password, hashed_password);
+    return result;
+  } catch (err) {
+    console.log("Invalid Password");
+    return false;
+  }
 };
 
 const signUpUser = async (req, res) => {
-	try {
-		const { error } = signUpSchema.validate(req.body);
-		if (error)
-			return res.status(400).json({ message: "Schema Validation Failed" });
+  try {
+    const { error } = signUpSchema.validate(req.body);
+    if (error)
+      return res.status(400).json({ message: "Schema Validation Failed" });
 
-		const existingUser = await userModel.findOne({ email: req.body.email });
-		if (existingUser) {
-			return res
-				.status(409)
-				.json({ message: "User already exists, please log in" });
-		}
-		hash = await hashPassword(req.body.password);
-		const newUser = new userModel({
-			Name: req.body.name,
-			email: req.body.email,
-			password: hash,
-			rollNumber: req.body.rollNo,
-			role: req.body.role ? req.body.role : "student",
-		});
+    const existingUser = await userModel.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "User already exists, please log in" });
+    }
+    hash = await hashPassword(req.body.password);
+    const newUser = new userModel({
+      Name: req.body.Name,
+      email: req.body.email,
+      password: hash,
+      rollNumber: req.body.rollNumber,
+      role: req.body.role ? req.body.role : "student",
+    });
 
-		const savedUser = await newUser.save();
-		const token = createToken(savedUser._id, savedUser.role);
+    const savedUser = await newUser.save();
+    const token = createToken(savedUser._id, savedUser.role);
 
-		res.cookie("token", token, {
-			path: "/", // Accessible across the app
-			httpOnly: false, // Prevent client-side access
-			secure: false, // Use `true` only for HTTPS
-			sameSite: "Lax", // Allow basic cross-origin
-			expire: new Date(Date.now() + 28800000),
-		});
-		return res.status(200).json({ savedUser, token });
-	} catch (err) {
-		res
-			.status(400)
-			.json({ message: "An Error Occured Please Try Again Later" });
-	}
+    res.cookie("token", token, {
+      path: "/", // Accessible across the app
+      httpOnly: false, // Prevent client-side access
+      secure: false, // Use `true` only for HTTPS
+      sameSite: "Lax", // Allow basic cross-origin
+      expire: new Date(Date.now() + 28800000),
+    });
+    return res.status(200).json({ savedUser, token });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ message: "An Error Occured Please Try Again Later" });
+  }
 };
 
 const loginUser = async (req, res) => {
-	try {
-		const { error } = loginSchema.validate(req.body);
-		if (error) {
-			return res
-				.status(400)
-				.json({ message: `Validation error: ${error.details[0].message}` });
-		}
-		const existingUser = await userModel.findOne({ email: req.body.email });
-		if (!existingUser) return res.status(404).send("User Not Found");
-		const password = req.body.password;
-		const result = await matchPassword(password, existingUser.password_hash);
-		if (!existingUser || !result) {
-			return res.status(409).json({ message: "Invalid Credentials" });
-		}
-		const token = createSecretToken(existingUser._id, existingUser.role);
-		res.cookie("token", token, {
-			path: "/", // Accessible across the app
-			httpOnly: false, // Prevent client-side access
-			secure: false, // Use `true` only for HTTPS
-			sameSite: "Lax", // Allow basic cross-origin
-			expires: new Date(Date.now() + 28800000), // Cookie will be removed after 8 hours
-		});
-		
-		console.log("Logged IN!");
-		res.json(existingUser);
-	} catch (err) {
-		console.log("Error");
-		res.status(401).json({ message: "An Error Occured please try again" });
-	}
+  try {
+    const { error } = loginSchema.validate(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ message: `Validation error: ${error.details[0].message}` });
+    }
+    const existingUser = await userModel.findOne({ email: req.body.email });
+    if (!existingUser) return res.status(404).send("User Not Found");
+    const password = req.body.password;
+    const result = await matchPassword(password, existingUser.password);
+    if (!existingUser || !result) {
+      return res.status(409).json({ message: "Invalid Credentials" });
+    }
+    const token = createToken(existingUser._id, existingUser.role);
+    res.cookie("token", token, {
+      path: "/", // Accessible across the app
+      httpOnly: false, // Prevent client-side access
+      secure: false, // Use `true` only for HTTPS
+      sameSite: "Lax", // Allow basic cross-origin
+      expires: new Date(Date.now() + 28800000), // Cookie will be removed after 8 hours
+    });
+
+    console.log("Logged IN!");
+    res.json(existingUser);
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "An Error Occured please try again" });
+  }
 };
 
-const authenticateUser = (req, res, next) => {
-	var token = req.headers?.cookie; // Safely access the token
+const authenticateUser = async (req, res, next) => {
+  var token = req.headers?.cookie; // Safely access the token
 
-	if (!token) {
-		return res.status(401).send("Access Denied: No Token Provided");
-	}
+  if (!token) {
+    return res.status(401).send("Access Denied: No Token Provided");
+  }
 
-	try {
-		const token = req.headers.cookie
-			.split("; ")
-			.find((item) => item.startsWith("token="))
-			?.split("=")[1];
-		if (!token) {
-			return res.status(401).json({ message: "Access Denied: Token Missing" });
-		}
+  try {
+    const token = req.headers.cookie
+      .split("; ")
+      .find((item) => item.startsWith("token="))
+      ?.split("=")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Access Denied: Token Missing" });
+    }
 
-		const verifiedUser = jwt.verify(token, process.env.SECRET_HASH_STRING);
-		req.user = verifiedUser;
-
-	} catch (err) {
-		return res.status(403).json({ message: "Invalid Token" });
-	}
+    const verifiedUser = jwt.verify(token, process.env.JWTSECRET);
+    req.user = verifiedUser;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Invalid Token" });
+  }
 };
 
 module.exports = { loginUser, signUpUser, authenticateUser };
