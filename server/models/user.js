d7de47c7da38efa39ab89b5c24ee651d2fb466a3
YashwantBhosale <yashwantbhosale07@gmail.com>
2025-01-18 01:58:50 +0530

    update: updated user schema ~ added role field

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	Name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		match: [
+			/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+			"Please Provide A Valid Email Address",
+		],
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		enum: ["student", "teacher", "admin"],
+		required: true,
+		default: "student",
+	},
+	rollNumber: {
+		type: Number,
+		required: true,
+	},
+	assignment: {},
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server/routes/userAuth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
+const {
+  loginUser,
+  signUpUser,
+  authenticateUser,
+} = require("../controllers/userController/UserAuth");
+
+router.route("/login").post(loginUser);
+router.route("/signup").post(signUpUser);
+
+router.get("/logout", (req, res) => {
+  res.cookie("token", "none", {
+    path: "/",
+    httpOnly: false,
+    secure: false,
+    sameSite: "Lax",
+    expires: new Date(Date.now() + 1000),
+  });
+
+  res.status(200).json({ message: "Logged Out" });
+});
+
+router.route("/authenticate-user").post(authenticateUser, (req, res) => {
+  console.log(req.user.id);
+  const foundUser = userModel.findById(req.user.id);
+  res.status(200).json(foundUser);
+});
+
+module.exports = router;
