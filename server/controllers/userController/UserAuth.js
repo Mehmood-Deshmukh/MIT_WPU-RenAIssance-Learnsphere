0313fdc329d4f4c0f37c97f45a103d6f089d135b
HardikMutha <hardikmutha2005@gmail.com>
2025-01-18 16:44:26 +0530

    Chore : Code Cleanup

--- server/controllers/userController/UserAuth.js
+++ server/controllers/userController/UserAuth.js
@@ -81,28 +81,4 @@ const loginUser = async (req, res) => {
   }
 };
 
-const authenticateUser = async (req, res, next) => {
-  var token = req.headers?.cookie; // Safely access the token
-
-  if (!token) {
-    return res.status(401).send("Access Denied: No Token Provided");
-  }
-
-  try {
-    const token = req.headers.cookie
-      .split("; ")
-      .find((item) => item.startsWith("token="))
-      ?.split("=")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Access Denied: Token Missing" });
-    }
-
-    const verifiedUser = jwt.verify(token, process.env.JWTSECRET);
-    req.user = verifiedUser;
-    next();
-  } catch (err) {
-    return res.status(403).json({ message: "Invalid Token" });
-  }
-};
-
 module.exports = { loginUser, signUpUser };
