642532e63fb7741bc39e8d9a7778f3a1cd7db3b0
HardikMutha <hardikmutha2005@gmail.com>
2025-01-18 13:35:00 +0530

    Added Signup and Login Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
+import Login from "./Pages/Login";
 const App = () => {
   return (
     <div>
+      <Login />
       <h1 className="text-3xl font-semibold">Hello World</h1>
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./Pages/Login.jsx";
+import Singup from "./Pages/Signup.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<Singup />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
