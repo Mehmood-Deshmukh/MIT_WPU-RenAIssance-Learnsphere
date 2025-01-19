27cadb5eada28222868de3c05bef8ef463d2034f
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2025-01-19 07:06:26 +0530

    add: assignments in usermodel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,42 +2,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  Name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Please Provide A Valid Email Address",
-    ],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  /*
+	Name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		match: [
+			/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+			"Please Provide A Valid Email Address",
+		],
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	/*
 		we may need to specify seperate schema for role
 		where we can define all the roles and their permissions
 	*/
-  role: {
-    type: String,
-    enum: ["student", "teacher", "admin"],
-    required: true,
-    default: "student",
-  },
-  rollNumber: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  assignments: {
+	role: {
+		type: String,
+		enum: ["student", "teacher", "admin"],
+		required: true,
+		default: "student",
+	},
+	rollNumber: {
+		type: Number,
+		required: true,
+	},
+	assignments: {
 		type: [Schema.Types.ObjectId],
 		ref: "Assignment",
 	},
+
 	// following fields are only for teacher
 	subjects: {
 		type: [String],
