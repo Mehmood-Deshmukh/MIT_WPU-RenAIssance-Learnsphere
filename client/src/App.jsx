26759bf57d353b3de00c0fd09349e76b8eaa9d4a
HardikMutha <hardikmutha2005@gmail.com>
2025-01-19 09:29:50 +0530

    Feat : Added Teacher Dashboard and Removed Random Elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Singup from "./Pages/Signup";
+import TeacherDashboard from "./components/TeacherDashboard";
+// import ViewEnrollmentRequest from "./components/ViewEnrollmentRequest";
 
 const App = () => {
   return (
@@ -11,6 +13,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<Singup />} />
+        <Route
+          path="/teacherdashboard/:courseid"
+          element={<TeacherDashboard />}
+        />
       </Routes>
     </>
   );
